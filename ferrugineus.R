a1c9fdb0df496c70eb8d76bb631107eaa2876f8c
Unknown <fvdenes@gmail.com>
2017-07-06 02:43:24 -0600

    Including code for model run with few habitat types

--- ferrugineus.R
+++ ferrugineus.R
@@ -1,5 +1,4 @@
-### Test changes ###
-## 2nd test changes ##
+
 
 ## ferrugineus
 site.data <- read.csv("C:/Users/voeroesd/Dropbox/EBD/Loros Patagonia/pat_site.csv")
@@ -9,12 +8,12 @@ obs_fer <- read.csv("C:/Users/voeroesd/Dropbox/EBD/Loros Patagonia/pat_obs_fer.c
 elevation <- site.data$elevation
 agroflorestal <- site.data$Agroforestal
 agroganadero <- site.data$Agroganadero
-aramix <- site.data$Aramix
-estepa <- site.data$Estepa
-matorral <- site.data$Matorral
-noto <- site.data$Noto
-otros <- site.data$Otros
-urbano <- site.data$Urbano
+#aramix <- site.data$Aramix
+#estepa <- site.data$Estepa
+#matorral <- site.data$Matorral
+#noto <- site.data$Noto
+#otros <- site.data$Otros
+#urbano <- site.data$Urbano
 
 nsites <- nrow(site.data)
 
@@ -32,7 +31,7 @@ xg <- seq(0,B,delta)
 midpt <- xg[-1] - delta/2 # get midpoint of distance intervals
 nD <- length(midpt) # how many intervals
 
-M <- 1000                        # Size of augmented data set is M
+M <- 4000                        # Size of augmented data set is M
 nz <- M-nrow(data)              # Number of "pseudo-groups" added
 y <- c(rep(1,nrow(data)),rep(0,nz))      # Indicator of capture (== 1 for all obs. groups)
 nind <- nrow(data)              # Number of observed groups
@@ -43,11 +42,91 @@ zst <- y                        # Starting values for data augmentation variable
 
 dclass <- c(dclass,rep(NA,nz))
 
+
+# (few habitats, just to see if code runs)
+
+# Bundle data and produce summary
+str(bugs.data <- list (y=y, B=B, nind=nind, nsites=nsites, midpt=midpt, delta=delta, dclass=dclass, site=site, nz=nz, nD=nD, groupsize=groupsize,elevation=elevation,agroflorestal=agroflorestal,agroganadero=agroganadero))
+
+
+# Define model in BUGS language (few habitats, just to see if code runs)
+cat("
+    model{ 
+    
+    # Prior distributions for model parameters
+    alpha0 ~ dunif(-2,2)              # groupsize intercept
+    alpha1 ~ dunif(-5,5)              # groupsize slope
+    beta0 ~ dunif(-10,10)             # intercept for log(lambda)
+    beta1 ~ dunif(-10,10)             # elevation slope
+    beta2 ~ dunif(-10,10)             # agroflorestal slope
+    beta3 ~ dunif(-10,10)             # agroganadero slope
+  
+    lambda.group ~ dgamma(0.1, 0.1)
+    
+    
+    # psi is a derived parameter for sites
+    psi <- sum(lambda[])/(nind+nz)
+    
+    # Individual level model: observations and process
+    
+    for(i in 1:(nind+nz)){
+    
+    z[i] ~ dbern(psi)                    # Data augmentation variables
+    dclass[i] ~ dcat(pi.probs[site[i],]) # Population distribution of distance class
+    mu[i] <- z[i]*p[site[i],dclass[i]]   # p depends on site and distance class
+    groupsize[i] ~ dpois(lambda.group)   # Group size is Poisson
+    
+    log(sigma[i]) <- alpha0 +  alpha1*groupsize[i]
+    y[i] ~ dbern(mu[i])
+    site[i] ~ dcat(site.probs[1:nsites]) # Population distribution among sites
+    zg[i]<- z[i]*(groupsize[i] + 1)      # Number of individuals in that group
+    }
+    
+    for(s in 1:nsites){                  # Loop over sites
+    # Construct cell probabilities for nD cells (distance classes)
+    for (g in 1:nD){                     # midpt[g] = midpoint of each cell; g is each distance class  
+    log(p[s,g]) <- -midpt[g]*midpt[g]/(2*sigma[s]*sigma[s])
+    pi[s,g] <- ((2*midpt[g])/(B*B))*delta
+    pi.probs[s,g] <- pi[s,g]/norm[s]
+    f[s,g] <- p[s,g] * pi[s,g]
+    fc[s,g] <- f[s,g]/pcap[s]          # Conditional probabilities
+    }
+    pcap[s] <- sum(f[s,])                # Capture prob. is the sum of all rectangular areas
+    norm[s] <- sum(pi[s,])
+    
+    # Model for population size of groups
+    N[s] ~ dpois(lambda[s])
+    log(lambda[s])<- beta0 + beta1*elevation[s] + beta2*agroflorestal[s] + beta3*agroganadero[s] 
+    site.probs[s]<- lambda[s]/sum(lambda[])
+    }
+    
+    # Derived quantities
+    G <- sum(z[])        # Total number of groups
+    Ntotal <- sum(zg[])  # Total population size (all groups combined)
+    }
+    ",fill=TRUE, file="model1.txt")
+
+
+# Load some libraries, define MCMC settings, inits function and parameters to save
+
+library("jagsUI")  # 
+ni <- 3000   ;   nb <- 500   ;   nt <- 2   ;   nc <- 3
+inits <- function(){list(alpha0=1, alpha1=0.5, beta0=1, beta1=-0.5,beta2=-0.5, z=zst)}
+params <- c("alpha0", "alpha1", "beta0", "beta1", "beta2", "psi", "Ntotal", "G", "lambda.group")
+
+
+# Call JAGS, check convergence and summarize posterior distributions
+out1 <- jags(bugs.data, inits, params, "model1.txt", n.thin=nt,  n.chains=nc, n.burnin=nb,n.iter=ni, parallel=FALSE)
+traceplot(out1)    
+print(out1, 3)
+
+
+
+# (all habitat types)
 # Bundle data and produce summary
 str(bugs.data <- list (y=y, B=B, nind=nind, nsites=nsites, midpt=midpt, delta=delta, dclass=dclass, site=site, nz=nz, nD=nD, groupsize=groupsize,elevation=elevation,agroflorestal=agroflorestal,agroganadero=agroganadero,aramix=aramix,estepa=estepa,matorral=matorral,noto=noto,otros=otros,urbano=urbano ))
 
-
-# Define model in BUGS langauge
+# Define model in BUGS language (all habitat types)
 cat("
     model{ 
     
